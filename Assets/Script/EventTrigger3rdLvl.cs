7474eb52135c5143aed68555a2a1fc8496f2a13d
terskayl <97208428+terskayl@users.noreply.github.com>
2023-02-26 12:33:05 -0500

    3rd level cutscene finsihed
    Added cutscene and transition back to main menu

---
+++ Assets/Script/EventTrigger3rdLvl.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class EventTrigger3rdLvl : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public SpriteRenderer sprite;
+    private int totalPlayer = 0;
+    public GameObject image;
+    public GameObject character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        //sprite.color = new Color(1f,1f,1f,0f);
+        sprite.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+    
+        //playEvent
+
+        videoPlayer.enabled = true;
+        videoPlayer.Play();
+        character.GetComponent<CharacterController>().enabled = false;
+        character.GetComponent<CharacterController>().StopMovement();
+        //character.GetComponent<Animator>().SetTrigger("Idle");
+        col.gameObject.SetActive(false);
+        StartCoroutine(justTwoSecs());
+        //sprite.enabled = true;
+        //sprite.color = new Color(1f,1f,1f,1f);
+    }
+
+    IEnumerator justTwoSecs() {
+        yield return new WaitForSeconds(1.25f);
+        sprite.enabled = true;
+        yield return new WaitForSeconds(6f);
+        image.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+}
+
