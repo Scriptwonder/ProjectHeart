5dd29eecb67422e6d00f653a7e4e7da091ff4ec4
Charles Wang <charleszw16@gmail.com>
2023-02-25 22:39:49 -0600

    implement pause menu*
    *except for the game actually pausing lmaoooo

--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -4,8 +4,46 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject character;
+    public GameObject levelOnePause;
+    public GameObject levelTwoPause;
+    private CharacterSystem system;
+    private int characterId;
+
+    void Start()
+    {
+        system = character.GetComponent<CharacterSystem>();
+        characterId = character.GetComponent<CharacterSystem>().characterId;
+        updateBar(characterId);
+    }
+
+    void Update()
+    {
+        characterId = character.GetComponent<CharacterSystem>().characterId;
+        updateBar(characterId);
+    }
+
+    public void RestartLevelButton()
+    {
+        system.restart();
+    }
+
     public void ExitLevelButton()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
+    private void updateBar(int id)
+    {
+        if (id == 0)
+        {
+            levelOnePause.SetActive(true);
+            levelTwoPause.SetActive(false);
+        }
+        else if (id == 1)
+        {
+            levelOnePause.SetActive(false);
+            levelTwoPause.SetActive(true);
+        }
     }
 }
