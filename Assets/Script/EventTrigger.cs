2d3a8bce8499b986976197995ea5e2c957ff47f4
terskayl <97208428+terskayl@users.noreply.github.com>
2023-02-26 09:20:40 -0500

    Cutscene and background for conjuc
    Cutscene added

--- Assets/Script/EventTrigger.cs
+++ Assets/Script/EventTrigger.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class EventTrigger : MonoBehaviour
 {
+    public VideoPlayer videoPlayer;
     private int totalPlayer = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.enabled = false;
     }
 
     // Update is called once per frame
@@ -27,7 +30,9 @@ public class EventTrigger : MonoBehaviour
             totalPlayer++;
         } else {
             //playEvent
-
+            Debug.Log(1);
+            videoPlayer.enabled = true;
+            videoPlayer.Play();
         }
     }
 }
